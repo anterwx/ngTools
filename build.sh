2823280fc23cd0f84a4622ff5d37dd9423c66986
王星 <675046199@qq.com>
2018-09-19 09:39:13 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-# 执行scripts/build/packing.js
 
 echo 'ng-packagr 开始打包'
 
+# 执行scripts/build/packing.js
 node --max_old_space_size=4096 scripts/build/packing util
-
+# 切换目录
 cd dist/package-list/
-
+# 执行打包
 npm pack
 
 echo '打包结束'
